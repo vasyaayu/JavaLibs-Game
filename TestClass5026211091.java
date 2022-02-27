163ab389f90a5d19f23076ca1ee4e060b783a8d2
vasyaayu <100083372+vasyaayu@users.noreply.github.com>
2022-02-27 09:47:02 +0700

    Add files via upload

---
+++ TestClass5026211091.java
@@ -0,0 +1,12 @@
+package com.example;
+
+
+public class TestClass5026211091 {
+
+    public static void main(String args[]) {
+        ComputeMethods5026211091 computeMethods = new ComputeMethods5026211091();
+        System.out.println("Temp in celsius is "+ computeMethods5026211091.fToC(100));
+        System.out.println("Hypotenuse is "+ computeMethods5026211091.hypotenuse(3,4));
+        System.out.println("The sum of the dice values is "+ computeMethods5026211091.roll());
+    }
+}
